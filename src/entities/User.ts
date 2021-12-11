b4938df56fb5c4434ae8fd9f0d3270ebdcaea262
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-12-11 09:54:05 +0700

    fix: set true of unique column user

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -4,13 +4,13 @@ import Post from './Post';
 
 @Entity({ name: 'users' })
 export default class User extends BaseModel {
-    @Column({ type: "varchar", length: 128, nullable: false })
+    @Column({ type: "varchar", length: 128, nullable: false, unique: true })
     username!: string;
 
     @Column({ type: "varchar", length: 128, nullable: false })
     name!: string;
 
-    @Column({ type: "varchar", length: 255, nullable: false })
+    @Column({ type: "varchar", length: 255, nullable: false, unique: true })
     email!: string;
 
     @Column({ type: "varchar", length: 255, nullable: false })
